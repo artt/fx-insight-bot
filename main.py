1d25a2deec409158bb949f2149fa43a8b1b85091
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-12-06 20:55:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import os
+import requests
 
 print("Hi there!")
-print(os.environ['FACEBOOK_PAGE_ID'])
+
+curdate = '2020-12-04'
+# curdate = datetime.now().strftime('%Y-%m-%d')
+params = {'start_period': curdate, 'end_period': curdate}
+headers = {'x-ibm-client-id': os.environ['BOT_API_KEY'],
+           'accept': "application/json"}
+
+r = requests.get('https://apigw1.bot.or.th//bot/public/Stat-ReferenceRate/v2/DAILY_REF_RATE/',
+                 params=params, headers=headers)
+tmp = r.json()['result']['data']['data_detail'][0]
+print(tmp)
