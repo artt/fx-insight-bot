18049c1695607f217863cedb39078521e2289183
Art Tosborvorn <art.tosborvorn@gmail.com>
2021-06-09 16:14:25 +0700

    x

--- dashboard.py
+++ dashboard.py
@@ -35,7 +35,7 @@ var_thb = get_thbref(curdate, curdate)
 
 if var_thb != var_thb:
     print('No data for today.')
-    print('::set-env name=RUN_ENV::notrade')
+    os.system('''echo "RUN_RESULT=notrade" >> $GITHUB_ENV''')
     sys.exit(0)
 
 tmp_endlastday = get_thbref_before(curdate)
